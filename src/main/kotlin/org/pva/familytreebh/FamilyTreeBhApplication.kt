02de2829206b5fd1b72a57ef57443dc8f65322e0
ViktorVx <victorptrv@yandex.ru>
2022-01-16 21:49:26 +0300

    Init commit

---
+++ src/main/kotlin/org/pva/familytreebh/FamilyTreeBhApplication.kt
@@ -0,0 +1,11 @@
+package org.pva.familytreebh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyTreeBhApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilyTreeBhApplication>(*args)
+}
